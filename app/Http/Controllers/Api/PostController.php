5b592877aa0908d1bf71a2d5a7c283de5750abba
Gabri1391 <chiarellogabriele@gmail.com>
2022-10-05 00:17:40 +0200

    posts order by date

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::orderBy('created_at', 'DESC')->get();
         return response()->json($posts);
     }
 
