7370b5b9aaf0f1a97501742fd75867d6a683798d
Gabri1391 <chiarellogabriele@gmail.com>
2022-10-04 17:39:38 +0200

    create AppHeader.vue

---
+++ resources/js/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<template>
+ <header>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Boolpress</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home </a>
+      </li>
+      <!-- <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a> 
+      </li> -->
+    </ul>
+  </div>
+</nav>
+ </header>
+</template>
+
+<script>
+   export default {
+    name: "AppHeader",
+    props: {
+        title:String,
+   }
+};
+</script>
