9fc599015d298c0884b75f3beb243e3d73bca6cd
Gabri1391 <chiarellogabriele@gmail.com>
2022-10-05 09:15:33 +0200

    fixed bug loader

---
+++ resources/js/components/AppLoader.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="loader-overlay">
+      <div class="spinner-border text-primary" role="status"></div>
+    </div>
+</template>
+  
+  <script>
+  export default {
+    name: "AppLoader",
+  };
+  </script>
+  
+  <style scoped lang="scss">
+  .loader-overlay {
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    position: fixed;
+    z-index: 9;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .spinner-border {
+      width: 130px;
+      height: 130px;
+    }
+  }
+  </style>
--- resources/js/components/PostsList.vue
+++ resources/js/components/PostsList.vue
@@ -1,41 +1,48 @@
 <template>
-    <section id="posts-list">
-        <h2 class="my-3">Posts</h2>
-        <div></div>
-        <div v-if="posts.length">
-            <PostCard v-for="post in posts" :key="post.id" :post="post" />
-        </div>
-        <h5 v-else>Non ci sono posts</h5>
-    </section>
+   <section id="posts-list">
+    <h2 class="my-3">Posts</h2>
+    <AppLoader v-if="isLoading" />
+    <div v-else>
+      <div v-if="posts.length">
+        <PostCard v-for="post in posts" :key="post.id" :post="post" />
+      </div>
+      <h5 v-else>Non ci sono posts</h5>
+    </div>
+  </section>
 </template>
 
 <script>
+import AppLoader from "./AppLoader.vue";
 import PostCard from "./PostCard.vue"
 export default {
     name: "PostsList",
-    data() {
-        return {
-            posts: [],
-        };
+  data() {
+    return {
+      posts: [],
+      isLoading: false,
+    };
+  },
+  components:{ 
+    PostCard, 
+    AppLoader 
+  },
+  methods: {
+    fetchPosts() {
+      this.isLoading = true;
+      axios
+        .get("http://localhost:8000/api/posts")
+        .then((res) => {
+          this.posts = res.data;
+        })
+        .catch((err) => {
+          console.error(err);
+        }).then(() => {
+          this.isLoading = false;
+        })
     },
-    components:{PostCard},
-    methods: {
-        fetchPosts() {
-            axios
-                .get("http://localhost:8000/api/posts")
-                .then((res) => {
-                    this.posts = res.data;
-                })
-                .catch((err) => {
-                    console.error(err);
-                })
-                .then(() => {
-                    console.log("Chiamata terminata");
-                });
-        },
-    },
-    mounted() {
-        this.fetchPosts();
-    },
+  },
+  mounted() {
+    this.fetchPosts();
+  },
 };
 </script>
